88c4a9d197bec7455ac3f637f0cfcfd26334838d
leonardo.b <leonardo.b@solutionbeyond.com>
2020-03-03 12:19:46 -0400

    STYLE on background

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,16 +2,15 @@ import React, { useState } from 'react';
 import { Drawer, NavBar, Icon, List } from 'antd-mobile';
 import { Link, useLocation } from 'react-router-dom';
 import { SmallBackground } from '../assets/SmallBackground';
+import { SidemenuBottom } from '../assets/SidemuBottom';
 import { BigBackground } from '../assets/BigBackground';
+import { IoMdText, IoIosPeople } from 'react-icons/io';
 
 const DrawerComponent: React.FC<{ background: 'sm' | 'bg' }> = ({ children, background = 'sm' }) => {
   const [isOpen, setIsOpen] = useState(false);
   const screens = [
-    { name: '/feed' },
-    { name: '/createPost' },
-    { name: '/register' },
-    { name: '/post' },
-    { name: '/communities' },
+    { route: '/feed', name: 'Feed', icon: () => <IoMdText style={{ color: '#95bf74', fontSize: '0.9rem', marginLeft: '10%', marginRight: '5%' }} /> },
+    { route: '/communities', name: 'Communities', icon: () => <IoIosPeople style={{ color: '#95bf74', fontSize: '0.9rem', marginLeft: '10%', marginRight: '5%'  }} /> },
   ];
   const location = useLocation()
   return (
@@ -25,16 +24,19 @@ const DrawerComponent: React.FC<{ background: 'sm' | 'bg' }> = ({ children, back
         style={{ minHeight: document.documentElement.clientHeight, flex: 1, marginTop: "15%" }}
         enableDragHandle
         contentStyle={{ color: 'black', textAlign: 'center', display: 'flex', flexDirection: 'column' }}
-        sidebar={<List>
+        sidebarStyle={{ width: '60%', display: 'flex'}}
+        sidebar={<List style={{ height: '100%', position: 'relative', flex: 1, display: 'flex', flexDirection: 'column' }}>
           {screens.map((i, index) => {
-            return (<List.Item key={index}>
-              <Link onClick={() => setIsOpen(false)} to={i.name} style={{ textDecoration: 'none' }}>
+            return (<div key={index} style={{ display: 'flex', alignItems: 'center', marginTop: '15%'}}>
+              {i.icon()}
+              <Link onClick={() => setIsOpen(false)} to={i.route} style={{ color: '#95bf74', fontSize: '0.9rem', textDecoration: 'none' }}>
                 {i.name.replace(/\//g, '')}
               </Link>
-            </List.Item>);
+            </div>);
           })}
+          <SidemenuBottom />
         </List>}
-        open={isOpen}
+        open={true}
         onOpenChange={(c) => setIsOpen(!isOpen)}
       >
         {children}
